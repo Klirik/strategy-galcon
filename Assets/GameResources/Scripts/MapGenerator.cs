4001b211496318fbeb0e3f87caf78d566a5f0431
boyan.evtushok <boyan.evtushok@greent.games>
2021-03-08 12:20:01 +0700

    initial

---
+++ Assets/GameResources/Scripts/MapGenerator.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+public class MapGenerator : MonoBehaviour
+{
+    private const float MIN_PLANET_SIZE = 0.5f;
+    private const float MAX_PLANET_SIZE = 3f;
+
+    public Planet PlanetTemplate = null;
+    public Transform PlanetsContainer = null;
+
+    public float Height = 600;
+    public float Width = 800;
+
+    public int OneMapStep = 10;
+
+    public int MaxCountPlanets = 10;
+
+    private void Start()
+    {
+        Height = 2 * Camera.main.orthographicSize;
+        Width = Height * Camera.main.aspect;
+
+        var countHori = 0;
+        var countVert = 0;
+
+        var h = Height / countHori;
+        var w = Width / countVert;
+
+        for (var indW = 0; indW < countHori; indW++)
+        {
+            for (var indH = 0; indH < countVert; indH++)
+            {
+                var position = new Vector2(indW * w + MAX_PLANET_SIZE, indH * h + MAX_PLANET_SIZE);
+                CreatePlanet(CalculatePlanetSize(position));
+            }
+        }
+    }
+
+    private float coef = 1f;
+
+    private void CreatePlanet(float planetSize)
+    {
+        var planet = Instantiate(PlanetTemplate, PlanetsContainer);
+        planet.transform.position = coef * Random.insideUnitCircle;
+
+        planet.Init(planetSize);
+    }
+    public float CalculatePlanetSize(Vector2 posInRect)
+    {
+        //TODO:
+        posInRect += Random.insideUnitCircle;
+
+        return Random.Range(MIN_PLANET_SIZE, MAX_PLANET_SIZE);
+    }
+}
+
---
+++ Assets/GameResources/Scripts/Planet.cs
@@ -0,0 +1,80 @@
+﻿using Shapes;
+using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// Информация об одной планете
+/// </summary>
+[RequireComponent(typeof(Disc))]
+public class Planet : MonoBehaviour
+{
+    private const int GENERATE_COUNT_SHIP_IN_SEC = 5;
+    private const int SEC = 1;
+
+    public int CurrentShipCount { get; private set; } = 0;
+
+    private float planetSize = 0;
+
+    private bool isActiveFactory = false;
+
+    private Disc shape;
+
+    private Coroutine workingFactory = null;
+
+    /// <summary>
+    /// Инициализировать планету
+    /// </summary>
+    public void Init(float size)
+    {
+        shape = GetComponent<Disc>();
+
+        planetSize = size;
+
+        shape.Radius = planetSize;
+
+        StartFactoryShip();
+    }
+
+    /// <summary>
+    /// Добавить корабли к счетчику планеты
+    /// </summary>
+    /// <param name="countShip"></param>
+    public void AddCountShip(int countShip)
+    {
+        CurrentShipCount += countShip;
+    }
+
+    /// <summary>
+    /// Отнять корабли от счетчика планеты
+    /// </summary>
+    /// <param name="countShip"></param>
+    public void RemoveCountShip(int countShip)
+    {
+        CurrentShipCount -= countShip;
+    }
+
+    private void StartFactoryShip()
+    {
+        if (workingFactory != null) StopCoroutine(workingFactory);
+        workingFactory = StartCoroutine(StartFactory());
+    }
+
+    private IEnumerator StartFactory()
+    {
+        isActiveFactory = true; 
+
+        while (isActiveFactory)
+        {
+            var wait = SEC / GENERATE_COUNT_SHIP_IN_SEC;
+            yield return new WaitForSeconds(wait);
+            CurrentShipCount++;
+        }
+    }
+
+    private void StopFactory()
+    {
+        isActiveFactory = false;
+
+        if (workingFactory != null) StopCoroutine(workingFactory);
+    }
+}
